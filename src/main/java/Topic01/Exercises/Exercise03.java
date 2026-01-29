f65985088c49bd4d784139b12e40fdd44b39bfe2
HerinOd <hieudo851@gmail.com>
2026-01-29 15:39:07 +0700

    A Sync Test #5

--- src/main/java/Topic01/Exercises/Exercise03.java
+++ src/main/java/Topic01/Exercises/Exercise03.java
@@ -2,7 +2,6 @@ package Topic01.Exercises;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
 /* Write a program that asks user to input a list of N integers using the keyboard.
 Then the program should sort the list in ascending order, and print out the result. */
 public class Exercise03 {
