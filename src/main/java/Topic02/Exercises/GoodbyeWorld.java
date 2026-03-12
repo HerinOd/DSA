27b615f490da8f9388f1e7fb34180aede73fb80e
HerinOd <hieudo851@gmail.com>
2026-03-12 16:15:33 +0700

    Add Topic 2 and 3

---
+++ src/main/java/Topic02/Exercises/GoodbyeWorld.java
@@ -0,0 +1,7 @@
+package Topic02.Exercises;
+
+public class GoodbyeWorld {
+    public static void main(String[] args) {
+        System.out.println("Goodbye World");
+    }
+}
---
+++ src/main/java/Topic03/Examples/GoodbyeWorld.java
@@ -0,0 +1,7 @@
+package Topic03.Examples;
+
+public class GoodbyeWorld {
+    public static void main(String[] args) {
+        System.out.println("Goodbye World");
+    }
+}
